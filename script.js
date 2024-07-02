dc808c88ed0378a8d736034579c4cafc6fc78133
blobby-bobby <bulle.ouvrard@gmail.com>
2024-07-02 20:54:52 +0200

    document script

--- script.js
+++ script.js
@@ -1,45 +1,45 @@
-let numberOfItems = 14;
+let numberOfPotions = 14;
 let meterElement = document.getElementById("stock");
-let spanElement = document.getElementById("numberOfItems");
+let numberOfPotionsSpanElement = document.getElementById("numberOfPotions");
 let buyPotionButton = document.getElementById("buyPotion");
-let totalNumOfDivs = document.querySelectorAll("div").length;
-let numDivSpan = document.getElementById("numberOfDivSpan");
 
-// Function to update meter and span content
-function updateMeter() {
-  // Ensure numberOfItems stays within the min and max values of the meter
-  numberOfItems = Math.max(0, Math.min(15, numberOfItems));
+/**
+ * Update the state of the potion stock and the <meter>.
+ * Is called on click on the "buy Potion" button.
+ */
+function updatePotionStock() {
+  // Ensure numberOfPotions stays within the min and max values of the meter
+  numberOfPotions = Math.max(0, Math.min(15, numberOfPotions));
 
   // Update value attribute of meter element
-  meterElement.setAttribute("value", numberOfItems.toString());
+  meterElement.setAttribute("value", numberOfPotions.toString());
 
   // Update text content inside the meter element
-  meterElement.textContent = `${numberOfItems} items (${(
-    (numberOfItems / 8) *
-    100
-  ).toFixed(1)}%)`;
-
-  // Update span content
-  if (numberOfItems === 1) {
-    spanElement.textContent = `${numberOfItems} potion left`;
-  } else if (numberOfItems === 0) {
-    spanElement.textContent = `Oupsie! No potions in stock!`;
+  if (numberOfPotions === 1) {
+    numberOfPotionsSpanElement.textContent = `${numberOfPotions} potion left`;
+  } else if (numberOfPotions === 0) {
+    numberOfPotionsSpanElement.textContent = `Oupsie! No potions in stock!`;
     buyPotionButton.disabled = true;
   } else {
-    spanElement.textContent = `${numberOfItems} potions left`;
+    numberOfPotionsSpanElement.textContent = `${numberOfPotions} potions left`;
   }
 }
 
-updateMeter();
+// Call the function to set the initial state
+updatePotionStock();
 
 // Function to handle buying a potion
 function handleBuyPotion() {
-  numberOfItems--;
-  updateMeter();
+  numberOfPotions--;
+  updatePotionStock();
 }
 
 // Add event listener to button
 buyPotionButton.addEventListener("click", handleBuyPotion);
 
-// Total number of div
+/**
+ * The number of divs in the page
+ */
+let totalNumOfDivs = document.querySelectorAll("div").length;
+let numDivSpan = document.getElementById("numberOfDivSpan");
 numDivSpan.textContent = totalNumOfDivs;
